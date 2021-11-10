b75d70211b9ed8312ee439eef5d0ea0b4b5347dc
Edgy-Floors <eflores@mail.bradley.edu>
2021-11-09 20:33:22 -0600

    fuck shit up

--- Source.cpp
+++ Source.cpp
@@ -13,90 +13,90 @@ int MAX_JOB_QUEUE = 30;
 //queue<int> POWER_USER_QUEUE;
 //queue<int> USER_QUEUE;
 
-int main() 
-{
-    setJobQueues();           /* Set up the priority job queues with chosen file and data structure */
-
-   // signal(SIGINT, wake_up);
-
-    if (pid = fork() > 0) 
-    {/* Parent, jobGenerator process */
-
-        jobGenerator(); /* generate random jobs and put them into the priority queues
-                                             The priority queues must be protected in a critical region */
-    }
-    else 
-    {/* Child, job scheduler process */
-
-        jobScheduler(); /* schedule and execute the jobs. */
-
-        exit(0);
-    }
-
-    // while ((wpid = wait(&status)) > 0);
-    return (1);
-}
-
-// must access
-void jobScheduler() 
-{
-        while (i < N) 
-        {           /* schedule and run maximum N jobs */
-            n = selectJob();      /* pick a job from the job priority queues */
-
-                if (n > 0) 
-                { /* valid job id */
-
-                    if (pid = fork() == 0) 
-                    { /* child worker process */
-                        executeJob();      /* execute the job */
-                        exit(0);
-                    }
-                }
-            i++;
-        }
-}
-
-void executeJob()
-{
-    if (/*serverqueue*/)
-    {
-
-    }
-    if (/*poweruserqueue*/)
-    {
-        sleep();
-        signal(SIGINT, wake_up);
-        // run
-    }
-    if (/*userqueue*/)
-    {
-        sleep(2);
-        // run
-    }
-}
-
-int selectJob()
-{
-    int n;
-
-    return n;
-}
-
-void setJobQueues()
-{
-
-}
-
-// must access
-void jobGenerator()
-{
-//needs access to fucking;
-//string it = "um um";
-//i cant find it.sort();
-//pertabricadactol;
-
-}
+//int main() 
+//{
+//    setJobQueues();           /* Set up the priority job queues with chosen file and data structure */
+//
+//   // signal(SIGINT, wake_up);
+//
+//    if (pid = fork() > 0) 
+//    {/* Parent, jobGenerator process */
+//
+//        jobGenerator(); /* generate random jobs and put them into the priority queues
+//                                             The priority queues must be protected in a critical region */
+//    }
+//    else 
+//    {/* Child, job scheduler process */
+//
+//        jobScheduler(); /* schedule and execute the jobs. */
+//
+//        exit(0);
+//    }
+//
+//    // while ((wpid = wait(&status)) > 0);
+//    return (1);
+//}
+//
+//// must access
+//void jobScheduler() 
+//{
+//        while (i < N) 
+//        {           /* schedule and run maximum N jobs */
+//            n = selectJob();      /* pick a job from the job priority queues */
+//
+//                if (n > 0) 
+//                { /* valid job id */
+//
+//                    if (pid = fork() == 0) 
+//                    { /* child worker process */
+//                        executeJob();      /* execute the job */
+//                        exit(0);
+//                    }
+//                }
+//            i++;
+//        }
+//}
+//
+//void executeJob()
+//{
+//    if (/*serverqueue*/)
+//    {
+//
+//    }
+//    if (/*poweruserqueue*/)
+//    {
+//        sleep();
+//        signal(SIGINT, wake_up);
+//        // run
+//    }
+//    if (/*userqueue*/)
+//    {
+//        sleep(2);
+//        // run
+//    }
+//}
+//
+//int selectJob()
+//{
+//    int n;
+//
+//    return n;
+//}
+//
+//void setJobQueues()
+//{
+//
+//}
+//
+//// must access
+//void jobGenerator()
+//{
+////needs access to fucking;
+////string it = "um um";
+////i cant find it.sort();
+////pertabricadactol;
+//
+//}
 
 string * queLoader (string fileName)
 {
@@ -113,7 +113,8 @@ string * queLoader (string fileName)
 
 }
 
-int main() {
+int main() 
+{
     int pid = 0;
     setJobQueues(); /* Set up the priority job queues with chosen file and/or data structure */
     if (pid = fork() > 0) { /* jobGenerator process */
