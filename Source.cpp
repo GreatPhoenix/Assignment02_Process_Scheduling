b4098ce06d31936d67014c8f42dca7fcbaacee54
lbarrettCIS497 <70545042+lbarrett-bradley@users.noreply.github.com>
2021-11-10 23:08:07 -0600

    done bitch

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@
 #include <time.h> //for generate random seed
 #include <fcntl.h>
 // include c++ header files
-#include <string>
+#include <string.h>
 #include <cstring>
 #include <sys/stat.h>
 #include <iostream>
@@ -14,7 +14,7 @@
 #include <signal.h>
 #include <sstream>
 #define N 100 //N is the number of the worker processes. You may increase N to 100 when your program runs correctly
-#define M 100 //M is the number of jobs. You may increase M to 50 when your program runs correctly
+#define M 50 //M is the number of jobs. You may increase M to 50 when your program runs correctly
 
 using namespace std;
 
@@ -25,21 +25,13 @@ string selectJob();
 void executeJob(string x, int pid);
 void wake_up(int s);
 void down(int* semid, char* semname);
-void up(int* semid, char* semname);
+void up(int semid, char* semname);
 queue<string> SERVER_QUEUE;
 queue<string> POWER_USER_QUEUE;
 queue<string> USER_QUEUE;
 string fileServerQueue = "serverQueue.txt";
 string filePowerUserQueue = "powerUserQueue.txt";
 string fileUserQueue = "userQueue.txt";
-
-char serverSemname[30];
-char powerUserSemname[30];
-char userSemname[30];
-
-strcpy(serverSemname, fileServerQueue);
-strcpy(powerUserSemname, filePowerUserQueue);
-strcpy(userSemname, fileUserQueue);
 
 int intr = 0;
 
@@ -118,14 +110,21 @@ void setJobQueues()
 
 void jobGenerator(int pid)
 {
+    int semid;
+    char semname[10];
+
+    strcpy(semname, "mutex");
+
+    down(&semid, semname);
+
     int i = 0, n = 0;
     cout << "jobGenerator: Use a loop to generate M jobs in the priority queue: \n";
     // initialize random seed
     srand(time(0));
     while (i < M) {
         // generate a random number between 1-100
-        //n = rand() % 100 + 1;
-        n++;
+        n = rand() % 100 + 1;
+        //n++;
         cout << "jobGenerator: Job number is : " << n << endl;
         // Put the job n into the priority queue
         // write jobs into file
@@ -133,30 +132,24 @@ void jobGenerator(int pid)
             //serverque
             
             SERVER_QUEUE.push(to_string(n));
-            down(&pid, 'serverQueue.txt');
             updateQue(SERVER_QUEUE,fileServerQueue);
-            up(&pid, "serverQueue.txt");
         }
         else if (n >= 30 && n <= 60){
             
             // Power Job
             
             POWER_USER_QUEUE.push(to_string(n));
-            down(&pid, "powerUserQueue.txt");
             updateQue(POWER_USER_QUEUE,filePowerUserQueue);
-            up(&pid, "powerUserQueue.txt");
 
         }
         else if (n >= 61 && n <= 100){
             if (n == 69){
-                cout << "nice" << endl;
+                cout << "nice" << endl; //this serves no function other than making us laugh
             }
             
             // user job
             USER_QUEUE.push(to_string(n));
-            down(&pid, "userQueue.txt");
             updateQue(USER_QUEUE,fileUserQueue);
-            up(&pid, "userQueue.txt");
         }
 
 
@@ -164,24 +157,25 @@ void jobGenerator(int pid)
         usleep(100); //100 can be adjusted to synchronize the job generation and job scheduling processes.
         i++;
     }
+
+    up(semid, semname);
 }
 
 void jobScheduler(int pid)
 {
+    int semid;
+    char semname[10];
+
+    strcpy(semname, "mutex");
+
     int i = 0;
     string n = "";
 
-    down(&pid, "serverQueue.txt");
+    down(&semid, semname);
+
     queLoader(fileServerQueue);
-    up(&pid, "serverQueue.txt");
-
-    down(&pid, "powerUserQueue.txt");
     queLoader(filePowerUserQueue);
-    up(&pid, "powerUserQueue.txt");
-
-    down(&pid, "userQueue.txt");
     queLoader(fileUserQueue);
-    up(&pid, "userQueue.txt");
 
     while (i < N) { /* schedule and run maximum N jobs */
 
@@ -196,6 +190,8 @@ void jobScheduler(int pid)
         }
         i++;
     }
+
+    up(semid, semname);
 }
 
 string selectJob()
@@ -267,7 +263,7 @@ void down(int* semid, char* semname)
 }
 
 // continue everything
-void up(int* semid, char* semname)
+void up(int semid, char* semname)
 {
     close(semid);
     unlink(semname);
